807fb9d340510b7e14a0c8b641da033f38d4f229
james <github email address>
2017-06-24 17:28:12 -0500

    Add user_spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,2 +1,12 @@
 require 'spec_helper'
 
+describe 'User' do
+  before do
+    @user = User.create(username: "testuser", email: "testuser@exmaple.com", password: "test")
+  end
+  
+  it 'has a secure password' do
+    expect(@user.authenticate("wrong")).to eq(false)
+    expect(@user.authenticate("test")).to eq(@user)
+  end
+end
