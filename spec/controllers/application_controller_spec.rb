9260de630d76124ce8b7977f0103897e5f4557ff
james <github email address>
2017-06-25 17:00:01 -0500

    Add more test to Application Controller Spec and Index page

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -1,12 +1,13 @@
 require 'spec_helper'
 
-describe ApplicationController do
+RSpec.describe ApplicationController, type: :controller do
   describe "Homepage" do
     it 'loads the homepage' do
       get '/'
       expect(last_response.status).to eq(200)
       expect(last_response.body).to include("Welcome to Workout Tracker")
-      expect(last_response.body).should have_link("SignUp", href="/signup")
+      expect(last_response.body).to include("<a href=\"/signup\">SignUp</a>")
+      expect(last_response.body).to include("<a href=\"/login\">LogIn</a>")
     end
   end
 end
