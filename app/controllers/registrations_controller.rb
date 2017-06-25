1b2b2392c8d3b1bab369d608b24c841ef5fc25ab
james <github email address>
2017-06-25 18:32:20 -0500

    Add get and post routes to Registrations Controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,2 +1,17 @@
 class RegistrationsController < ApplicationController
+
+  get '/signup' do
+    erb :'/signup/new'
+  end
+
+  post '/signup' do
+    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    if @user.save
+      session[:user_id] = @user.id
+      redirect to '/workouts'
+    else
+      flash[:message] = "All fields are required."
+      redirect to '/signup'   
+    end
+  end
 end
--- spec/controllers/registrations_controller_spec.rb
+++ spec/controllers/registrations_controller_spec.rb
@@ -9,50 +9,54 @@ RSpec.describe RegistrationsController, type: :controller do
     
     it 'loads the signup page' do
       expect(last_response.status).to eq(200)  
+      expect(last_response.body).to include("username")
+      expect(last_response.body).to include("email")
+      expect(last_response.body).to include("password")
+      expect(last_response.body).to include("Submit")
     end
 
-    # it 'allows a user to signup and direct them to their workouts'
-    #   params = {
-    #     username: "testuser",
-    #     email: "testuser@example.com",
-    #     password: "test"
-    #   }
-    #   post '/signup', params
-    #   expect(last_response.location).to include('/workouts')
-    # end
+    it 'allows a user to signup and direct them to their workouts' do
+      params = {
+        username: "testuser",
+        email: "testuser@example.com",
+        password: "test"
+      }
+      post '/signup', params
+      expect(last_response.location).to include('/workouts')
+    end
 
-    # it 'does not allow user to signup without a username' do
-    #         params = {
-    #     username: "",
-    #     email: "testuser@example.com",
-    #     password: "test"
-    #   }
-    #   post '/signup', params
-    #   expect(last_response.body).to include("All fields are required.")
-    #   expect(last_response.location).to include('/signup')
-    # end
+    it 'does not allow user to signup without a username' do
+            params = {
+        username: "",
+        email: "testuser@example.com",
+        password: "test"
+      }
+      post '/signup', params
+      expect(last_response.body).to include("All fields are required.")
+      expect(last_response.location).to include('/signup')
+    end
 
-    # it 'does not allow user to signup without a email' do
-    #         params = {
-    #     username: "testuser",
-    #     email: "",
-    #     password: "test"
-    #   }
-    #   post '/signup', params
-    #   expect(last_response.body).to include("All fields are required.")
-    #   expect(last_response.location).to include('/signup')
-    # end
+    it 'does not allow user to signup without a email' do
+            params = {
+        username: "testuser",
+        email: "",
+        password: "test"
+      }
+      post '/signup', params
+      expect(last_response.body).to include("All fields are required.")
+      expect(last_response.location).to include('/signup')
+    end
 
-    # it 'does not allow user to signup without a password' do
-    #         params = {
-    #     username: "testuser",
-    #     email: "testuser@example.com",
-    #     password: ""
-    #   }
-    #   post '/signup', params
-    #   expect(last_response.body).to include("All fields are required.")
-    #   expect(last_response.location).to include('/signup')
-    # end
+    it 'does not allow user to signup without a password' do
+            params = {
+        username: "testuser",
+        email: "testuser@example.com",
+        password: ""
+      }
+      post '/signup', params
+      expect(last_response.body).to include("All fields are required.")
+      expect(last_response.location).to include('/signup')
+    end
 
   end
 end
