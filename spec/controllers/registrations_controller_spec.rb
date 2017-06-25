d40f8a1860b8021d06e36c3314cc54fb0eacaff3
james <github email address>
2017-06-25 17:09:44 -0500

    Add test to Registrations Controller spec

--- spec/controllers/registrations_controller_spec.rb
+++ spec/controllers/registrations_controller_spec.rb
@@ -1,8 +1,25 @@
 require 'spec_helper'
 
-describe RegistrationsController do
+RSpec.describe RegistrationsController, type: :controller do
 
   describe "Signup Page" do
+    before do
+      get '/signup'
+    end
+    
+    it 'loads the signup page' do
+      expect(last_response.status).to eq(200)  
+    end
+
+    it 'allows a user to signup and direct them to their dashboard'
+      params = {
+        username: "testuser",
+        email: "testuser@example.com",
+        password: "test"
+      }
+      post '/signup', params
+      expect(last_response.location).to include("/workouts")
+    end
     
   end
   
