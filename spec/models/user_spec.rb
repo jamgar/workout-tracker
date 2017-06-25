5b2f938a7b0134727f4b8ce2aa9630d2c2f2f5ca
james <github email address>
2017-06-25 15:21:40 -0500

    Add config.ru file

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,12 +1,19 @@
 require 'spec_helper'
 
-describe 'User' do
-  before do
-    @user = User.create(username: "testuser", email: "testuser@exmaple.com", password: "test")
+RSpec.describe User, type: model do
+
+  context "validations" do
+    it "is invalid without a username" do
+      expec(User.create(username: nil, email: ""))
+    end
   end
   
-  it 'has a secure password' do
-    expect(@user.authenticate("wrong")).to eq(false)
-    expect(@user.authenticate("test")).to eq(@user)
-  end
+  # before do
+  #   @user = User.create(username: "testuser", email: "testuser@exmaple.com", password: "test")
+  # end
+  
+  # it 'has a secure password' do
+  #   expect(@user.authenticate("wrong")).to eq(false)
+  #   expect(@user.authenticate("test")).to eq(@user)
+  # end
 end
