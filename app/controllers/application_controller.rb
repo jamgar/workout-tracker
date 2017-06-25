53d4aeb964d384ff191666ea68885cd893702a1f
james <github email address>
2017-06-25 15:32:02 -0500

    Fixed typos to get first test to run correct

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
-require 'config/environment'
+require './config/environment'
 
 class ApplicationController < Sinatra::Base
-  config do
+  configure do
     set :public_folder, 'public'
     set :view, 'app/views'
     enable :sessions
