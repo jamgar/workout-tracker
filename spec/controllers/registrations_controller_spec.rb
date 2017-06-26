01fd7fff4a7dbca7ba31a7b0c752ccbbc0931d5a
james <github email address>
2017-06-26 08:46:09 -0500

    Fixed registration spec

--- spec/controllers/registrations_controller_spec.rb
+++ spec/controllers/registrations_controller_spec.rb
@@ -26,36 +26,36 @@ RSpec.describe RegistrationsController, type: :controller do
     end
 
     it 'does not allow user to signup without a username' do
-            params = {
+      params = {
         username: "",
         email: "testuser@example.com",
         password: "test"
       }
       post '/signup', params
+      expect(last_request.path).to eq('/signup')
       expect(last_response.body).to include("All fields are required.")
-      expect(last_response.location).to include('/signup')
     end
 
     it 'does not allow user to signup without a email' do
-            params = {
+      params = {
         username: "testuser",
         email: "",
         password: "test"
       }
       post '/signup', params
+      expect(last_request.path).to eq('/signup')
       expect(last_response.body).to include("All fields are required.")
-      expect(last_response.location).to include('/signup')
     end
 
     it 'does not allow user to signup without a password' do
-            params = {
+      params = {
         username: "testuser",
         email: "testuser@example.com",
         password: ""
       }
       post '/signup', params
+      expect(last_request.path).to eq('/signup')
       expect(last_response.body).to include("All fields are required.")
-      expect(last_response.location).to include('/signup')
     end
 
   end
