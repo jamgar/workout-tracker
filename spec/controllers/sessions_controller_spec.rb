383663828eb155364532f1ae23e2499d9a199f87
james <github email address>
2017-06-26 11:54:31 -0500

    Add Sign in test to sessions_controller_spec

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -19,7 +19,27 @@ RSpec.describe SessionsController, type: :controller do
 
   describe "Sign In" do
     before do
-  
+      @user1 = User.create(username: "testuser1", email: "testuser1@example.com", password: "test1")
+      @user2 = User.create(username: "testuser2", email: "testuser2@example.com", password: "test2")
+      @user3 = User.create(username: "testuser3", email: "testuser3@example.com", password: "test3")
+    end
+
+    it "returns a 302 redirect status code" do
+      params = {
+        username: "testuser1",
+        password: "test1"
+      }
+      post '/login', params
+      expect(last_response.status).to eq(302)
+    end
+
+    it "sets session[:user_id] equal to id of the user" do
+      params = {
+        username: "testuser2",
+        password: "test2'"
+      }
+      post '/login', params
+      expect(session[:user_id]).to eq(2)
     end
   end
 end
