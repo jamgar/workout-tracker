2c278724130fc94516927b540e9e4918a667dfaf
james <github email address>
2017-06-29 06:26:32 -0500

    Add new workout and add an new exercise test

--- spec/features/workouts_controller_spec.rb
+++ spec/features/workouts_controller_spec.rb
@@ -6,19 +6,31 @@ describe "Workout Forms" do
       visit '/workouts/new'
     end
 
-    context "creates a new workout on submit" do
+    it "creates a new workout on submit" do
+      exercise = Exercise.create(name: "Running")
       fill_in "Title", with: "Hill Repteats"
       fill_in "Date", with: "06/01/2017"
       fill_in "Duration(min)", with: 40
-      check "Running"
+      check exercise.name
       fill_in "Note", "I am tired now."
       click_on "Create"
 
       expect(page).to have_content("Hill Repeats")
       expect(page).to have_content("06/01/2017")
       expect(page).to have_content(40)
-      expect(page).to have_content("Running")
+      expect(page).to have_content(exercise.name)
       expect(page).to have_content("I am tired now.")
+    end
+
+    it "adds a new exercise on create a new workout" do
+      expect {
+        fill_in "Title", with: "100s"
+        fill_in "Date", with: "06/01/2017"
+        fill_in "Duration(min)", with: 40
+        fill_in "Exercise", with: "Swimming"
+        fill_in "Note", "I swam at the gym."
+        click_on "Create"
+      }.to change(Exercise, :count).by(1)
     end
   end
 end
