bb4c048e74850505e23677ee204b1ba3722d8732
james <github email address>
2017-06-30 18:11:49 -0500

    Add /login to sessions_controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,10 @@
 class SessionsController < ApplicationController
-
+  get '/login' do
+    if !logged_in?
+      erb :'/session/index'
+    else
+      redirect to '/workouts'
+    end
+  end
+  
 end
