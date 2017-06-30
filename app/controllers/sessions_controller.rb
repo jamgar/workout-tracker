73e5fb86d3f05759285bce0d7719cc08f101481e
james <github email address>
2017-06-30 18:44:48 -0500

    Add post /login to sessions_controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,5 +6,17 @@ class SessionsController < ApplicationController
       redirect to '/workouts'
     end
   end
+
+  post '/login' do
+    @user = User.find_by(username: params[:username])
+
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect to '/workouts'
+    else
+      flash[:message] = "Incorrect username or password. Try again."
+      redirect to '/login'
+    end
+  end
   
 end
