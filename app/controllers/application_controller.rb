c36b8428612ef74321c46dfd4018bb1ced8ec13c
james <github email address>
2017-06-30 17:28:43 -0500

    Add application_controller and add configure settings

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,12 @@
+require './config/environment'
+require 'rack-flash'
+
+class ApplicationController < Sinatra::Base
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "workout_secret"
+    use Rack::Flash
+  end
+end
