5b3b42695e34ed9304221b57b3fcc1172fcdc7d6
james <github email address>
2017-06-30 17:48:16 -0500

    Add registrations_controller_spec

---
+++ spec/controllers/registrations_controller_spec.rb
@@ -0,0 +1,61 @@
+require 'spec_helper'
+
+RSpec.describe RegistrationsController, type: :controller do
+
+  describe "Signup Page" do
+    before do
+      get '/signup'
+    end
+    
+    it 'loads the signup page' do
+      expect(last_response.status).to eq(200)  
+      expect(last_response.body).to include("username")
+      expect(last_response.body).to include("email")
+      expect(last_response.body).to include("password")
+      expect(last_response.body).to include("Submit")
+    end
+
+    it 'allows a user to signup and direct them to their workouts' do
+      params = {
+        username: "testuser",
+        email: "testuser@example.com",
+        password: "test"
+      }
+      post '/signup', params
+      expect(last_response.location).to include('/workouts')
+    end
+
+    it 'does not allow user to signup without a username' do
+      params = {
+        username: "",
+        email: "testuser@example.com",
+        password: "test"
+      }
+      post '/signup', params
+      expect(last_request.path).to eq('/signup')
+      expect(last_response.location).to include('/signup')
+    end
+
+    it 'does not allow user to signup without a email' do
+      params = {
+        username: "testuser",
+        email: "",
+        password: "test"
+      }
+      post '/signup', params
+      expect(last_request.path).to eq('/signup')
+      expect(last_response.location).to include('/signup')
+    end
+
+    it 'does not allow user to signup without a password' do
+      params = {
+        username: "testuser",
+        email: "testuser@example.com",
+        password: ""
+      }
+      post '/signup', params
+      expect(last_request.path).to eq('/signup')
+      expect(last_response.location).to include('/signup')
+    end
+  end
+end
