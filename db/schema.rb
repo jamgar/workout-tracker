c564af5f80ac021198c08fad0c330236469bd56b
james <github email address>
2017-06-30 17:10:40 -0500

    Create Exercise migration and model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170630215842) do
+ActiveRecord::Schema.define(version: 20170630220811) do
+
+  create_table "exercises", force: :cascade do |t|
+    t.string "name"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
