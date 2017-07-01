58ec2067dfb34c5a559c09ec3f703fb03eaa3b17
james <github email address>
2017-07-01 07:53:18 -0500

    Add /workouts/:id and /workouts/:id/edit routes to workouts_controller

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -15,4 +15,22 @@ class WorkoutsController < ApplicationController
       redirect to '/login'
     end
   end
+
+  get '/workouts/:id' do
+    if logged_in?
+      @workout = Workout.find_by(id: params[:id])
+      erb :'/workouts/show'
+    else
+      redirect to '/login'
+    end
+  end
+
+  get '/workouts/:id/edit' do
+    if logged_in?
+      @workout = Workout.find_by(id: params[:id])
+      erb :'/workouts/edit'
+    else
+      redirect to '/login'
+    end
+  end
 end
